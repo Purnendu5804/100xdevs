c964b13771eb1c210c607175959adea809428a90
Purnendu Tiwari <purnendutiwari2004@gmail.com>
2025-03-26 23:47:59 +0530

    add week2 nodejs assignment

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -17,5 +17,30 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
+app.get("/files" , function(req ,res) {
+  fs.readdir(path.join(__dirname,"./files") , function(err ,files) {
+    if (err) {
+      return res.status(500).json({
+        msg : "Invalid Directory!"
+      });
+    }
+    res.json(files);
+  });
+});
+
+
+app.get("/file/:filename" , function(req ,res) {
+  const filePath = path.join(__dirname , "./files/" , req.params.filename);
+  fs.readFile(filePath , "utf-8" , function(err ,data) {
+    if (err) {
+      return res.status(404).send("File not found")
+    }
+    res.send(data);
+  });
+});
+
+app.all("*" , (req ,res) => {
+  res.status(404).send("Route not found")
+});
 
 module.exports = app;
--- week-2/02-nodejs/todoServer.js
+++ week-2/02-nodejs/todoServer.js
@@ -45,5 +45,70 @@
   const app = express();
   
   app.use(bodyParser.json());
+
+  let todos = []
+
+  //GET request to retrieve all the todos
+  app.get("/todos" , function(req ,res) {
+    res.json(todos);
+  });
+
+  app.get("/todos/:id" , (req,res) => {
+    const todo = todos.find((reqtodo) => reqtodo.id  === parseInt(req.params.id))
+
+    if(!todo) {
+      res.status(404).json({
+        msg : "Todo doesn't exists !"
+      });
+    }
+
+    res.json(todo);
+  })
+
+  app.post("todos" , (req ,res) => {
+    const newTodo = {
+      id : Math.floor(Math.random() * 1000000) ,
+      title : req.body.title,
+      description : req.body.description
+    };
+
+    todos.push(newTodo);
+    res.status(201).json(newTodo);
+  });
+
+  app.put("todos/:id" ,(req ,res) => {
+    const todoIndex = todos.findIndex(
+      (reqTodo) => reqTodo.id === parseInt(req.params.id)
+    );
+
+    if(todoIndex == -1) {
+      res.status(404).json({
+        msg : "Todo doesn't exists !"
+      })
+    } else {
+      todos[todoIndex].title = req.body.title;
+      todos[todoIndex].description = req.body.description;
+      res.json(todos[todoIndex])
+    }
+  });
+
+  app.delete("/todos/id" , (req ,res) => {
+    const todoIndex = todos.findIndex(
+      (reqTodo) => reqTodo.id === parseInt(req.params.id)
+    );
+
+    if (todoIndex == -1) {
+      res.status.json({
+        msg : "Todo doesn't exits !!"
+      })
+    } else {
+      todos.splice(todoIndex , 1);
+      res.status(200).send();
+    }
+  });
+
+  app.use((req ,res ,next) => {
+    res.status(404).send("Route not found !!")
+  })
   
   module.exports = app;
